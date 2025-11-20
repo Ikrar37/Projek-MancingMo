b42ac11c51803f00a44518e424f162c991a1a8b9
Muhammad Ikrar <muhammadikrar225@MacBook-Air-Muhammad.local>
2025-11-20 18:43:05 +0800

    Enable migration in build script

--- build_files.sh
+++ build_files.sh
@@ -6,4 +6,7 @@ pip install -r requirements.txt
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
 
+echo "Running migrations..."
+python manage.py migrate --noinput
+
 echo "Build complete!"
