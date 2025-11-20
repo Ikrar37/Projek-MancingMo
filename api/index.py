72e7e4cbbb4e8cd14d31482f50e1b39490dcdd50
Muhammad Ikrar <muhammadikrar225@MacBook-Air-Muhammad.local>
2025-11-20 18:16:31 +0800

    deploy

--- api/index.py
+++ api/index.py
@@ -1,6 +1,5 @@
 from ecommerce.wsgi import application
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
-def handler(request, context):
-    return application(request, context)
+# Vercel requirement
+app = application
